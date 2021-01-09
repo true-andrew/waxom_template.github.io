a115324bd3471b03b5363138080946324c308c66
true-andrew <75199537+true-andrew@users.noreply.github.com>
2021-01-09 21:01:36 +0300

    Скрипт для запуска видео

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+document.addEventListener('click', (event) => {
+    if (event.target === document.getElementById("play_video_icon") || event.target === document.getElementById("play_video_icon_svg") || event.target === document.getElementById("play_video_icon_path")) {
+        if (!document.getElementById("video_presentation").paused) {
+            document.getElementById("video_presentation").pause();
+        } else {
+            document.getElementById("video_presentation").play();
+        }
+    }
+})
