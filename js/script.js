50044b20c8020c7831a43177afa0474ff4adba87
true-andrew <75199537+true-andrew@users.noreply.github.com>
2021-01-18 21:25:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ document.addEventListener('click', (event) => {
     if (event.target === document.getElementById("play_video_icon") || event.target === document.getElementById("play_video_icon_svg") || event.target === document.getElementById("play_video_icon_path")) {
         if (!document.getElementById("video_presentation").paused) {
             document.getElementById("video_presentation").pause();
+            document.getElementById("mobile_video_presentation").pause();
         } else {
             document.getElementById("video_presentation").play();
+            document.getElementById("mobile_video_presentation").play();
         }
     }
 })
